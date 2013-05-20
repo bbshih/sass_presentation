75499e87e6e1d9fd92a9ad0a36c48148df2c83b3
Billy Shih <me@billyshih.com>
2013-05-20 15:54:53 -0700

    Added variables, nesting and mixin examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SassPresentation::Application.routes.draw do
   resources :habits
 
+  root :to => "habits#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
